6790dd0172f4b63d523839ba629c085427e5d114
brianpinto91 <brianpinto91@gmail.com>
2020-11-19 20:40:44 +0100

    update utils.py

--- training/utils.py
+++ training/utils.py
@@ -1,25 +1,17 @@
 import os
+import subprocess
 import pandas as pd
 from PIL import Image
-
+import h5py
+import json
+import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, models
 
-data_dir = "./data"
-train_dir = os.path.join(data_dir, "train")
-test_dir = os.path.join(data_dir, "test")
-train_labels_df = pd.read_csv(os.path.join(data_dir, "train_labels.csv"))
-test_labels_df = pd.read_csv(os.path.join(data_dir, "test_labels.csv"))
-breeds_class_df = pd.read_csv(os.path.join(data_dir,
-                              "breeds_to_class_map.csv"))
-
-breeds_to_class = {x: y for x, y in zip(breeds_class_df.breed.values.tolist(),
-                   breeds_class_df.class_id.values.tolist())}
-class_to_breeds = {y: x for y, x in enumerate(breeds_to_class)}
 
 data_transform = transforms.Compose([
-        transforms.RandomResizedCrop(225),
+        transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.476, 0.452, 0.392],
@@ -28,26 +20,22 @@ data_transform = transforms.Compose([
 
 
 class ImageDataset(Dataset):
-    def __init__(self, image_folder, labels_file, return_with_labels,
+    def __init__(self, h5_file_path,
                  transformer=data_transform):
-        self.image_folder = image_folder
-        self.labels_file = labels_file
-        self.transformer = transformer
-        self.return_with_labels = return_with_labels
+        h5_file = h5py.File(h5_file_path, 'r', libver='latest', swmr=True)
+        self.labels = h5_file['labels'][:]
+        self.features = h5_file['data'][:]
+        h5_file.close()
+        self.transformer = data_transform
 
     def __len__(self):
-        return self.labels_file.shape[0]
+        return self.labels.shape[0]
 
     def __getitem__(self, idx):
-        image_name_i = self.labels_file.iloc[idx]['id'] + '.jpg'
-        image_i = Image.open(os.path.join(self.image_folder, image_name_i))
+        image_i = Image.fromarray(self.features[idx])
         image_i = self.transformer(image_i)
-        if self.return_with_labels:
-            breed_i = self.labels_file.iloc[idx]['breed']
-            label_i = breeds_to_class[breed_i]
-            return image_name_i, image_i, label_i
-        else:
-            return image_name_i, image_i
+        label_i = int(self.labels[idx])
+        return image_i, label_i
 
 
 def ImageLoader(dataset, batch_size, shuffle=True):
@@ -61,3 +49,58 @@ def Model(pretrained=True):
     in_fea = model.fc.in_features
     model.fc = nn.Linear(in_fea, 120)
     return model
+
+
+def get_data_paths(args):
+    if args.model_dir[0:2] == "gs":
+        copy_gcloud_data(args)
+    train_file_path = os.path.join("../data", 'train.h5')
+    test_file_path = os.path.join("../data", 'test.h5')
+    return train_file_path, test_file_path
+
+def copy_gcloud_data(args):
+    try:
+        subprocess.check_call(['gsutil', 'cp', os.path.join(args.data_dir, "*"), "../data"])
+    except Exception as e:
+        print(e)
+        print("Could not fetch the data from the gcloud bucket. Check Check if the cloud bucket and path exist!")
+
+def save_model(args, model):
+    model_name = "torch_model"
+    local_save_path = os.path.join("../models", model_name)
+    torch.save(model.state_dict(), local_save_path)
+    if args.model_dir[0:2] == "gs":
+        try:
+            subprocess.check_call(['gsutil', 'cp', local_save_path,
+                                  os.path.join(args.model_dir, model_name)])
+        except Exception as e:
+            print(e)
+            print("Could not save the model to the cloud. Check if the cloud bucket and path exist!")
+
+
+def save_job_log(args, log_df):
+    log_file_name = "training_result.csv"
+    local_save_path = os.path.join("../logs", log_file_name)
+    log_df.to_csv(local_save_path, index=False)
+    if args.log_dir[0:2] == "gs":
+        try:
+            subprocess.check_call(['gsutil', 'cp', local_save_path,
+                                  os.path.join(args.log_dir, log_file_name)])
+        except Exception as e:
+            print(e)
+            print("Could not save the log file to the cloud. Check if the cloud bucket and path exist!")
+
+
+def save_train_metadata(args, best_performance_metrics):
+    metadata_file_name = "metadata.txt"
+    local_save_path = os.path.join("../logs", metadata_file_name)
+    json_args = json.dumps(vars(args))
+    with open(local_save_path, 'w') as outfile:
+        json.dump(json_args, outfile)
+    if args.log_dir[0:2] == "gs":
+        try:
+            subprocess.check_call(['gsutil', 'cp', local_save_path,
+                                  os.path.join(args.log_dir, metadata_file_name)])
+        except Exception as e:
+            print(e)
+            print("Could not save the metadata file to the cloud. Check if the cloud bucket and path exist!")
